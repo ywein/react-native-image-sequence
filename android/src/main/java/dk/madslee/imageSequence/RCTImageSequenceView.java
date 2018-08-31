9e2d1ae8d73a7c8eb5ba9a43716dd2322dca3a7d
Ywein <trueblamed@gmail.com>
2018-08-31 18:37:44 +0300

    Update RCTImageSequenceView.java

--- android/src/main/java/dk/madslee/imageSequence/RCTImageSequenceView.java
+++ android/src/main/java/dk/madslee/imageSequence/RCTImageSequenceView.java
@@ -1,13 +1,17 @@
 package dk.madslee.imageSequence;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
-import android.util.Log;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.ImageView;
+
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,6 +23,8 @@ import java.util.concurrent.RejectedExecutionException;
 
 public class RCTImageSequenceView extends ImageView {
     private Integer framesPerSecond = 24;
+    private Integer width = null;
+    private Integer height = null;
     private Boolean loop = true;
     private ArrayList<AsyncTask> activeTasks;
     private HashMap<Integer, Bitmap> bitmaps;
@@ -26,7 +32,6 @@ public class RCTImageSequenceView extends ImageView {
 
     public RCTImageSequenceView(Context context) {
         super(context);
-
         resourceDrawableIdHelper = new RCTResourceDrawableIdHelper();
     }
 
@@ -52,7 +57,13 @@ public class RCTImageSequenceView extends ImageView {
 
 
         private Bitmap loadBitmapByLocalResource(String uri) {
-            return BitmapFactory.decodeResource(this.context.getResources(), resourceDrawableIdHelper.getResourceDrawableId(this.context, uri));
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            Bitmap bitmap = BitmapFactory.decodeResource(this.context.getResources(), resourceDrawableIdHelper.getResourceDrawableId(this.context, uri));
+            int imageHeight = options.outHeight;
+            int imageWidth = options.outWidth;
+            String imageType = options.outMimeType;
+            return decodeSampledBitmapFromResource(this.context.getResources(), resourceDrawableIdHelper.getResourceDrawableId(this.context, uri), width, height);
         }
 
         private Bitmap loadBitmapByExternalURL(String uri) {
@@ -76,6 +87,8 @@ public class RCTImageSequenceView extends ImageView {
         }
     }
 
+    
+
     private void onTaskCompleted(DownloadImageTask downloadImageTask, Integer index, Bitmap bitmap) {
         if (index == 0) {
             // first image should be displayed as soon as possible.
@@ -88,6 +101,14 @@ public class RCTImageSequenceView extends ImageView {
         if (activeTasks.isEmpty()) {
             setupAnimationDrawable();
         }
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
     }
 
     public void setImages(ArrayList<String> uris) {
@@ -140,9 +161,51 @@ public class RCTImageSequenceView extends ImageView {
         return activeTasks != null && !activeTasks.isEmpty();
     }
 
+    public static int calculateInSampleSize(
+        BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+        int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
     private void setupAnimationDrawable() {
         AnimationDrawable animationDrawable = new AnimationDrawable();
+
+
         for (int index = 0; index < bitmaps.size(); index++) {
+            Bitmap bitmap = bitmaps.get(index);
             BitmapDrawable drawable = new BitmapDrawable(this.getResources(), bitmaps.get(index));
             animationDrawable.addFrame(drawable, 1000 / framesPerSecond);
         }
@@ -151,5 +214,21 @@ public class RCTImageSequenceView extends ImageView {
 
         this.setImageDrawable(animationDrawable);
         animationDrawable.start();
+
+        new Thread( new Runnable() {
+            public void run()  {
+                try  { Thread.sleep( bitmaps.size() * (1000 / framesPerSecond) + 500 ); }
+                catch (InterruptedException ie)  {}
+                // WritableMap event = Arguments.createMap();
+                // event.putString("message", true);
+                ReactContext reactContext = (ReactContext)getContext();
+                reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+                getId(),
+                "onEnd",
+                null);
+            }
+        } ).start();
+
+        
     }
 }
